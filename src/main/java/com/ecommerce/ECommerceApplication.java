08242637e39db673e23243c5b2f33e5181c0090f
Amgaa <enhuushm518@gmail.com>
2024-12-09 04:17:29 +0800

    Update ECommerceApplication

--- src/main/java/com/ecommerce/ECommerceApplication.java
+++ src/main/java/com/ecommerce/ECommerceApplication.java
@@ -1,7 +1,11 @@
 package com.ecommerce;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ECommerceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ECommerceApplication.class, args);
     }
 }
