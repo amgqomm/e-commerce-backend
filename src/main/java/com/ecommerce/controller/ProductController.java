101723be12a307b9121d103feb828bd1e244c96c
Amgaa <enhuushm518@gmail.com>
2024-12-09 04:12:04 +0800

    Create controller

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -1,4 +1,42 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.dto.ProductDTO;
+import com.ecommerce.entity.Product;
+import com.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    @Autowired
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/top-rated")
+    public List<ProductDTO> getTopRatedProducts() {
+        return productService.getTopRatedProducts();
+    }
+
+    @GetMapping
+    public List<ProductDTO> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDTO productDTO) {
+        Product product = productService.saveProduct(productDTO);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}")
+    public ProductDTO getProductDetails(@PathVariable Long id) {
+        return productService.getProductDetails(id);
+    }
 }
--- src/main/java/com/ecommerce/controller/ProductImageController.java
+++ src/main/java/com/ecommerce/controller/ProductImageController.java
@@ -1,4 +1,25 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.entity.ProductImage;
+import com.ecommerce.service.ProductImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products-images")
 public class ProductImageController {
+
+    @Autowired
+    private ProductImageService productImageService;
+
+    @GetMapping
+    public List<ProductImage> getAllProductsImages() {
+        return productImageService.getAllProductsImage();
+    }
+
+    @PostMapping
+    public ProductImage createProductImages(@RequestBody ProductImage productImages) {
+        return productImageService.createProductImage(productImages);
+    }
 }
